2314cfdfd89fc44bc51d226cfe6235304b0de1fb
“labdatabase” <howardcruzroatti@gmail.com>
2023-09-25 09:33:35 -0300

    começadno alterações

---
+++ src/utils/splash_screen.py
@@ -0,0 +1,51 @@
+from conexion.oracle_queries import OracleQueries
+from utils import config
+
+class SplashScreen:
+
+    def __init__(self):
+        # Consultas de contagem de registros - inicio
+        self.qry_total_clientes = config.QUERY_COUNT.format(tabela="clientes")
+        self.qry_total_pedidos = config.QUERY_COUNT.format(tabela="pedidos")
+        # Consultas de contagem de registros - fim
+
+        # Nome(s) do(s) criador(es)
+        self.created_by = "Estêvão Paulo Pereira"
+        self.professor = "Prof. M.Sc. Howard Roatti"
+        self.disciplina = "Banco de Dados"
+        self.semestre = "2023/2"
+
+    def get_total_clientes(self):
+        # Cria uma nova conexão com o banco que permite alteração
+        oracle = OracleQueries()
+        oracle.connect()
+        # Retorna o total de registros computado pela query
+        return oracle.sqlToDataFrame(self.qry_total_clientes)["total_clientes"].values[0]
+
+    def get_total_pedidos(self):
+        # Cria uma nova conexão com o banco que permite alteração
+        oracle = OracleQueries()
+        oracle.connect()
+        # Retorna o total de registros computado pela query
+        return oracle.sqlToDataFrame(self.qry_total_pedidos)["total_pedidos"].values[0]
+
+    def get_updated_screen(self):
+        return f"""
+        ########################################################
+        #                   SISTEMA DE VENDAS                     
+        #                                                         
+        #  TOTAL DE REGISTROS:                                    
+        #      1 - PRODUTOS:         {str(self.get_total_produtos()).rjust(5)}
+        #      2 - CLIENTES:         {str(self.get_total_clientes()).rjust(5)}
+        #      3 - FORNECEDORES:     {str(self.get_total_fornecedores()).rjust(5)}
+        #      4 - PEDIDOS:          {str(self.get_total_pedidos()).rjust(5)}
+        #      5 - ITENS DE PEDIDOS: {str(self.get_total_itens_pedidos()).rjust(5)}
+        #
+        #  CRIADO POR: {self.created_by}
+        #
+        #  PROFESSOR:  {self.professor}
+        #
+        #  DISCIPLINA: {self.disciplina}
+        #              {self.semestre}
+        ########################################################
+        """
