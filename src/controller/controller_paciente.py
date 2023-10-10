a6a12be2a795a3f6c0e1ed95cf6c7decd8a1f036
estevao.pereira <estevaopp05@gmail.com>
2023-10-09 21:32:25 -0300

    add telefone

--- src/controller/controller_paciente.py
+++ src/controller/controller_paciente.py
@@ -18,12 +18,14 @@ class Controller_Paciente:
         if self.verifica_existencia_paciente(oracle, cpf):
             # Solicita ao usuario o novo nome
             nome = input("Nome (Novo): ")
+            # Solicita ao usuario o novo telefone
+            telefone = input("Telefone (Novo): ")
             # Insere e persiste o novo paciente
-            oracle.write(f"insert into pacientes values ('{cpf}', '{nome}')")
+            oracle.write(f"insert into pacientes values ('{cpf}', '{nome}', '{telefone})")
             # Recupera os dados do novo paciente criado transformando em um DataFrame
-            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome from pacientes where cpf = '{cpf}'")
+            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome, telefone from pacientes where cpf = '{cpf}'")
             # Cria um novo objeto Paciente
-            novo_paciente = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0])
+            novo_paciente = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0], df_paciente.telefone.values[0])
             # Exibe os atributos do novo paciente
             print(novo_paciente.to_string())
             # Retorna o objeto novo_paciente para utilização posterior, caso necessário
@@ -38,18 +40,20 @@ class Controller_Paciente:
         oracle.connect()
 
         # Solicita ao usuário o código do paciente a ser alterado
-        cpf = int(input("CPF do paciente que deseja alterar o nome: "))
+        cpf = int(input("CPF do paciente que deseja alterar o nome e telefone: "))
 
         # Verifica se o paciente existe na base de dados
         if not self.verifica_existencia_paciente(oracle, cpf):
             # Solicita a nova descrição do paciente
             novo_nome = input("Nome (Novo): ")
+            # Solicita ao usuario o novo telefone
+            telefone = input("Telefone (Novo): ")
             # Atualiza o nome do paciente existente
-            oracle.write(f"update pacientes set nome = '{novo_nome}' where cpf = {cpf}")
+            oracle.write(f"update pacientes set nome = '{novo_nome}', telefone = '{telefone}' where cpf = {cpf}")
             # Recupera os dados do novo paciente criado transformando em um DataFrame
-            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome from pacientes where cpf = {cpf}")
+            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome, telefone from pacientes where cpf = {cpf}")
             # Cria um novo objeto paciente
-            paciente_atualizado = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0])
+            paciente_atualizado = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0], df_paciente.telefone.values[0])
             # Exibe os atributos do novo paciente
             print(paciente_atualizado.to_string())
             # Retorna o objeto paciente_atualizado para utilização posterior, caso necessário
@@ -69,11 +73,11 @@ class Controller_Paciente:
         # Verifica se o paciente existe na base de dados
         if not self.verifica_existencia_paciente(oracle, cpf):            
             # Recupera os dados do novo paciente criado transformando em um DataFrame
-            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome from pacientes where cpf = {cpf}")
+            df_paciente = oracle.sqlToDataFrame(f"select cpf, nome, telefone from pacientes where cpf = {cpf}")
             # Revome o paciente da tabela
             oracle.write(f"delete from pacientes where cpf = {cpf}")            
             # Cria um novo objeto Paciente para informar que foi removido
-            paciente_excluido = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0])
+            paciente_excluido = Paciente(df_paciente.cpf.values[0], df_paciente.nome.values[0], df_paciente.telefone.values[0])
             # Exibe os atributos do paciente excluído
             print("Paciente Removido com Sucesso!")
             print(paciente_excluido.to_string())
@@ -82,5 +86,5 @@ class Controller_Paciente:
 
     def verifica_existencia_paciente(self, oracle:OracleQueries, cpf:str=None) -> bool:
         # Recupera os dados do novo paciente criado transformando em um DataFrame
-        df_paciente = oracle.sqlToDataFrame(f"select cpf, nome from pacientes where cpf = {cpf}")
+        df_paciente = oracle.sqlToDataFrame(f"select cpf, nome, telefone from pacientes where cpf = {cpf}")
         return df_paciente.empty
